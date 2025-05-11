15f0076cee419d2b83de582ccfb2704a272aaefe
Arnavghimire <ghimirearnav738@gmail.com>
2025-05-11 23:59:13 +0545

    add it

---
+++ que1.py
@@ -0,0 +1,16 @@
+# def integer(number):
+#     list_of_integer=[]
+#     for i in number:
+#         i=int(i)
+#         list_of_integer.append(i)
+#     largest=max(list_of_integer)
+#     second_largest = float('-inf')
+#     for number in list_of_integer:
+#         if number!=largest and number>second_largest:
+#             second_largest=number
+#     print(second_largest)
+            
+        
+# integer(['45', '23', '89', '12'])
+second_largest = float('-inf')
+print(second_largest)
